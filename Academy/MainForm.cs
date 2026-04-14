4b1a799f5ec720f2d0fad52c7bf4e7ea0d8dad47
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-04-14 22:14:51 +0300

    edit

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -145,6 +145,8 @@ namespace Academy
 			
 			AddForm addForm = new AddForm();
 			addForm.ShowDialog();
+			if (addForm.ShowDialog() == DialogResult.OK)
+				tabControl_SelectedIndexChanged(tabControl, null);
 		}
 		
 	}
