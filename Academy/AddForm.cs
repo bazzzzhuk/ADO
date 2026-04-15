a30c9f93fe41e2b6f561ff768ca42656d4057d3e
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-04-15 19:13:31 +0300

    group edit

--- Academy/AddForm.cs
+++ Academy/AddForm.cs
@@ -107,7 +107,7 @@ namespace Academy
 			string columnes = "";
 			string dateB = "";
 			string columnes_val = string.Empty;
-			for (int i = 1; i < flp.Controls.Count; i++)
+			for (int i = 0; i < flp.Controls.Count; i++)
 			{
 				if (flp.Controls[i].Controls[flp.Controls[i].Name == "birth_date" ? 0 : 1].Text == string.Empty) continue;
 				//int io = (flp.Controls[i].Name == "birth_date" || flp.Controls[i].Name == "group") ? 0 : 1;
