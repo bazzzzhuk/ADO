1c1c4778fc3de90182cba0f2740fd9eb00a2afb8
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-04-27 00:38:10 +0300

    Quest #1 Done

--- AcademyWPF/MainWindow.xaml.cs
+++ AcademyWPF/MainWindow.xaml.cs
@@ -33,23 +33,52 @@ namespace AcademyWPF
 		{
 			InitializeComponent();
 			connector = new Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
-			tables = new DataGrid[] {dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers};
+			tables = new DataGrid[] { dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers };
 			tabControl.SelectedIndex = 0;
 			d_directions = connector.GetDictionary("Directions");
 			d_groups = connector.GetDictionary("Groups");
-  
-			foreach(var dict  in d_groups)
+
+			foreach (var dict in d_groups)
 				cb_StudentsGroups.Items.Add(dict.Key);
-			foreach(var dict  in d_directions)
+			foreach (var dict in d_directions)
 				cb_StudentsDirections.Items.Add(dict.Key);
-			}
+		}
 
 		private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+			cb_StudentsGroups.SelectedIndex = -1;
+			cb_StudentsDirections.SelectedIndex = -1;
 			int i = (sender as TabControl).SelectedIndex;
 			tables[i].ItemsSource = connector.
 				Select($"SELECT * FROM {((sender as TabControl).Items[i] as TabItem).Header.ToString()}").DefaultView;
+			if(tabControl.SelectedIndex==1)cb_StudentsGroups.IsEnabled = false;
+			if(tabControl.SelectedIndex==1)cb_StudentsGroups.Visibility = Visibility.Collapsed;
 			StatusBarCount.Text = $"Количество записей: {tables[i].Items.Count - 1}";
 		}
-    }
+
+		private void cb_StudentsDirections_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			string s = (tabControl.SelectedValue as TabItem).Header.ToString();
+			string fields = "";
+			string tables_select="";
+			string where_select = "";
+
+			//MessageBox.Show(s.ToString());
+			switch (s)
+			{
+				case "Students":
+					fields = "stud_id,last_name,first_name,middle_name,group_name,direction_name";
+					tables_select = "Students, Groups, Directions";
+					where_select = "[group]=group_id AND direction=direction_id";
+					break;
+				case "Groups":
+					fields = "*";
+					tables_select = "Groups,Directions";
+					where_select= "direction=direction_id";
+					break;
+			}
+			tables[tabControl.SelectedIndex].ItemsSource = connector.
+				Select($"SELECT {fields} FROM {tables_select} WHERE {where_select} AND direction={cb_StudentsDirections.SelectedIndex + 1}").DefaultView;
+		}
+	}
 }
